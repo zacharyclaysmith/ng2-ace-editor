f7caaa6cfb0e26b41107e4eb16945e39ed7144db
Francois-Xavier Montigny <francois-xavier@use-design.com>
2016-08-10 15:46:10 +0200

    Add .js.map and .d.ts files

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+import { EventEmitter, ElementRef } from '@angular/core';
+import 'brace';
+import 'brace/theme/monokai';
+import 'brace/mode/html';
+export declare class AceEditorDirective {
+    textChanged: EventEmitter<{}>;
+    _options: any;
+    _readOnly: boolean;
+    _theme: string;
+    _mode: string;
+    editor: any;
+    oldText: any;
+    constructor(elementRef: ElementRef);
+    init(): void;
+    initEvents(): void;
+    options: any;
+    readOnly: any;
+    theme: any;
+    mode: any;
+    text: any;
+}
